a22efb54fe02b049ad45ea4e306e0e58e80b203a
pictoune <piriouvictor@gmail.com>
2023-11-21 18:53:14 +0100

    cleaned, optimized, modularized

--- PW_object_tracking/code/hough.py
+++ PW_object_tracking/code/hough.py
@@ -1,145 +1,134 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy import signal
 import cv2
+from config import VIDEO_PATHS
 
-def get_angle(vec):    
-    i,j = vec
-    angle = 0
-    if(j == 0):
-        if(i < 0):
-            angle = np.pi/2
-        elif(i > 0):
-            angle = -np.pi/2
-    elif(i == 0):
-        if(j < 0):
-            angle = np.pi
-        elif(j > 0):
-            angle = 0
-    elif(i == 0 and j == 0):
-        angle = 0
-    elif(i < 0 and j > 0):
-        angle = np.arctan(-i/j)
-    elif(i < 0 and j < 0):
-        angle = np.pi - np.arctan(i/j)
-    elif(i > 0 and j < 0):
-        angle = np.pi + np.arctan(-i/j)
-    else:
-        angle = -np.arctan(i/j)
-        
-    return np.rad2deg(angle)
+from utils import ROI, compute_grad, process_roi
 
-def compute_grad(I):
 
-    hy=np.array([[1/2,1,1/2]])
-    hx=np.array([[-1/2,0,1/2]])
-    Ix = signal.convolve2d(I, hy.T@hx)
-    Iy = signal.convolve2d(I, hx.T@hy)
+def process_video(path):
+    """
+    Processes a video by tracking an object within a defined region of interest (ROI).
 
-    mod = np.sqrt(np.square(Ix)+np.square(Iy))
-    ori = np.zeros(mod.shape)
-    for i in range(mod.shape[0]):
-        for j in range(mod.shape[1]):
-            ori[i,j] = get_angle((Ix[i,j],Iy[i,j]))
+    Args:
+        path: The path to the video file.
+    """
+    cap = cv2.VideoCapture(path)
 
-    return mod, ori
+    # Check if the video capture object was successfully created
+    if not cap.isOpened():
+        print(f"Error: Could not open video file {path}.")
+        return
 
-roi_defined = False
- 
-def define_ROI(event, x, y, flags, param):
-	global r,c,w,h,roi_defined
-	# if the left mouse button was clicked, 
-	# record the starting ROI coordinates 
-	if event == cv2.EVENT_LBUTTONDOWN:
-		r, c = x, y
-		roi_defined = False
-	# if the left mouse button was released,
-	# record the ROI coordinates and dimensions
-	elif event == cv2.EVENT_LBUTTONUP:
-		r2, c2 = x, y
-		h = abs(r2-r)
-		w = abs(c2-c)
-		r = min(r,r2)
-		c = min(c,c2)  
-		roi_defined = True
-
-# cap = cv2.VideoCapture('./VOT-Ball.mp4')
-# cap = cv2.VideoCapture('./Antoine_Mug.mp4')
-# cap = cv2.VideoCapture('./VOT-Sunshade.mp4')
-# cap = cv2.VideoCapture('./VOT-Basket.mp4')
-# cap = cv2.VideoCapture('./VOT-Car.mp4')
-cap = cv2.VideoCapture('./VOT-Woman.mp4')
-
-# take first frame of the video
-ret,frame = cap.read()
-# load the image, clone it, and setup the mouse callback function
-clone = frame.copy()
-cv2.namedWindow("First image")
-cv2.namedWindow("orientations")
-cv2.namedWindow("Hough transform")
-cv2.setMouseCallback("First image", define_ROI)
- 
-# keep looping until the 'q' key is pressed
-while True:
-	# display the image and wait for a keypress
-	cv2.imshow("First image", frame)
-	key = cv2.waitKey(1) & 0xFF
-	# if the ROI is defined, draw it!
-	if (roi_defined):
-		# draw a green rectangle around the region of interest
-		cv2.rectangle(frame, (r,c), (r+h,c+w), (0, 255, 0), 2)
-	# else reset the image...
-	else:
-		frame = clone.copy()
-	# if the 'q' key is pressed, break from the loop
-	if key == ord("q"):
-		break
- 
-track_window = (r,c,h,w)
-Rtable = {}
-for k in range(21):
-    Rtable[k]=[]
-# set up the ROI for tracking
-roi = frame[r:r+h,c:c+w]
-center_x = c+w/2
-center_y=r+h/2
-clone = cv2.cvtColor(clone, cv2.COLOR_BGR2GRAY)
-mod,ori = compute_grad(clone)
-ori+=90
-discret = 360/20
-for i in range(roi.shape[0]):
-    for j in range(roi.shape[1]):
-        if mod[r+i,c+j]>20:
-            Rtable[int(ori[r+i,c+j]/discret)].append((i-center_y+r,j-center_x+c))
-
-while(1):
-    key = cv2.waitKey(1)
-    ret, frame = cap.read()
-    if ret==True:
-        # print('center',center_x,center_y)
+    try:
+        # take first frame of the video
+        ret, frame = cap.read()
+        # load the image, clone it, and setup the mouse callback function
         clone = frame.copy()
+        cv2.namedWindow("First image")
+        cv2.namedWindow("orientations")
+        cv2.namedWindow("Hough transform")
+        myROI = ROI()
+        cv2.setMouseCallback("First image", myROI.define_ROI)
+
+        # keep looping until the 'q' key is pressed
+        while True:
+            # display the image and wait for a keypress
+            cv2.imshow("First image", frame)
+            key = cv2.waitKey(1) & 0xFF
+            frame = process_roi(frame, myROI, clone)
+            # if the 'q' key is pressed, break from the loop
+            if key == ord("q"):
+                break
+
+        Rtable = {k: [] for k in range(21)}
+        # set up the ROI for tracking
+        roi = frame[myROI.r : myROI.r + myROI.h, myROI.c : myROI.c + myROI.w]
+        center_x = myROI.c + myROI.w / 2
+        center_y = myROI.r + myROI.h / 2
         clone = cv2.cvtColor(clone, cv2.COLOR_BGR2GRAY)
-        mod,ori = compute_grad(clone)
-        ori+=90
-        H = np.zeros(ori.shape)
-        for l in range(ori.shape[0]):
-            for m in range(ori.shape[1]):
-                if mod[l,m] > 20:
-                    for ind in Rtable[int(ori[l,m]/discret)]:
-                        if l+ind[0]>= 0 and l+ind[0]<H.shape[0] and m+ind[1]>=0 and m+ind[1]<H.shape[1]:
-                            H[int(l+ind[0]),int(m+ind[1])]+=1
-        cv2.imshow("Hough transform", H/H.max())
-        center_x, center_y = np.unravel_index(np.argmax(H), H.shape)
-        # print(int(center_y-h/2),int(center_x-w/2),int(center_y+h/2),int(center_x+w/2))
-        cv2.rectangle(frame, (int(center_y-h/2),int(center_x-w/2)), (int(center_y+h/2),int(center_x+w/2)), (0, 255, 0), 2)
-        cv2.imshow("First image", frame)
+        mod, ori = compute_grad(clone)
+        ori += 90
+        discret = 360 / 20
 
-        #display valid orientations
-        ori = cv2.cvtColor(ori.astype('uint8'), cv2.COLOR_GRAY2BGR)
-        ori[np.where(mod<20)] = [0,0,255]
-        cv2.imshow("orientations", ori)
-    else:
-        break
+        # Create indices for the entire ROI
+        roi_indices = np.indices(roi.shape)
+        i_indices, j_indices = roi_indices[0], roi_indices[1]
 
-cv2.destroyAllWindows()
-cap.release()
+        # Offset indices to match the position in the original image
+        i_indices += myROI.r
+        j_indices += myROI.c
+
+        # Flatten the indices for vectorized operations
+        i_flat = i_indices.flatten()
+        j_flat = j_indices.flatten()
+
+        # Filter indices based on the condition
+        filtered_indices = mod[i_indices, j_indices] > 20
+        i_filtered = i_flat[filtered_indices]
+        j_filtered = j_flat[filtered_indices]
+
+        # Compute the orientation and the displacement
+        orientations = ori[i_filtered, j_filtered]
+        displacements = np.stack([(i_filtered - center_y, j_filtered - center_x)], axis=-1)
+
+        # Map the filtered values to Rtable
+        for orient, disp in zip(orientations, displacements):
+            Rtable[int(orient / discret)].append(tuple(disp))
+
+        while True:
+            key = cv2.waitKey(1)
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            clone = frame.copy()
+            clone = cv2.cvtColor(clone, cv2.COLOR_BGR2GRAY)
+            mod, ori = compute_grad(clone)
+            ori += 90
+            H = np.zeros(ori.shape)
+
+            for l in range(ori.shape[0]):
+                for m in range(ori.shape[1]):
+                    if mod[l, m] > 20:
+                        for ind in Rtable[int(ori[l, m] / discret)]:
+                            if (
+                                0 <= l + ind[0] < H.shape[0]
+                                and 0 <= m + ind[1] < H.shape[1]
+                            ):
+                                H[int(l + ind[0]), int(m + ind[1])] += 1
+
+            cv2.imshow("Hough transform", H / H.max())
+            center_x, center_y = np.unravel_index(np.argmax(H), H.shape)
+            cv2.rectangle(
+                frame,
+                (int(center_y - myROI.h / 2), int(center_x - myROI.w / 2)),
+                (int(center_y + myROI.h / 2), int(center_x + myROI.w / 2)),
+                (0, 255, 0),
+                2,
+            )
+            cv2.imshow("First image", frame)
+
+            ori = cv2.cvtColor(ori.astype("uint8"), cv2.COLOR_GRAY2BGR)
+            ori[np.where(mod < 20)] = [0, 0, 255]
+            cv2.imshow("orientations", ori)
+    
+    except KeyboardInterrupt:
+        print("Interrupted by user, releasing resources.")
+    
+    finally:
+        cv2.destroyAllWindows()
+        cap.release()
+
+
+if __name__ == "__main__":
+    paths = {
+        0: "data/VOT-Ball.mp4",
+        1: "data/Antoine_Mug.mp4",
+        2: "data/VOT-Sunshade.mp4",
+        3: "data/VOT-Basket.mp4",
+        4: "data/VOT-Car.mp4",
+        5: "data/VOT-Woman.mp4",
+    }
+
+    process_video(VIDEO_PATHS[0])
--- PW_object_tracking/code/mix.py
+++ PW_object_tracking/code/mix.py
@@ -1,170 +1,150 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy import signal
 import cv2
+from config import VIDEO_PATHS
 
-def get_angle(vec):    
-    i,j = vec
-    angle = 0
-    if(j == 0):
-        if(i < 0):
-            angle = np.pi/2
-        elif(i > 0):
-            angle = -np.pi/2
-    elif(i == 0):
-        if(j < 0):
-            angle = np.pi
-        elif(j > 0):
-            angle = 0
-    elif(i == 0 and j == 0):
-        angle = 0
-    elif(i < 0 and j > 0):
-        angle = np.arctan(-i/j)
-    elif(i < 0 and j < 0):
-        angle = np.pi - np.arctan(i/j)
-    elif(i > 0 and j < 0):
-        angle = np.pi + np.arctan(-i/j)
-    else:
-        angle = -np.arctan(i/j)
-        
-    return np.rad2deg(angle)
-
-def compute_grad(I):
-
-    hy=np.array([[1/2,1,1/2]])
-    hx=np.array([[-1/2,0,1/2]])
-    Ix = signal.convolve2d(I, hy.T@hx)
-    Iy = signal.convolve2d(I, hx.T@hy)
-
-    mod = np.sqrt(np.square(Ix)+np.square(Iy))
-    ori = np.zeros(mod.shape)
-    for i in range(mod.shape[0]):
-        for j in range(mod.shape[1]):
-            ori[i,j] = get_angle((Ix[i,j],Iy[i,j]))
-
-    return mod, ori
-
-roi_defined = False
- 
-def define_ROI(event, x, y, flags, param):
-	global r,c,w,h,roi_defined
-	# if the left mouse button was clicked, 
-	# record the starting ROI coordinates 
-	if event == cv2.EVENT_LBUTTONDOWN:
-		r, c = x, y
-		roi_defined = False
-	# if the left mouse button was released,
-	# record the ROI coordinates and dimensions
-	elif event == cv2.EVENT_LBUTTONUP:
-		r2, c2 = x, y
-		h = abs(r2-r)
-		w = abs(c2-c)
-		r = min(r,r2)
-		c = min(c,c2)  
-		roi_defined = True
-
-# cap = cv2.VideoCapture('./VOT-Ball.mp4')
-# cap = cv2.VideoCapture('./Antoine_Mug.mp4')
-# cap = cv2.VideoCapture('./VOT-Sunshade.mp4')
-cap = cv2.VideoCapture('./VOT-Basket.mp4')
-# cap = cv2.VideoCapture('./VOT-Car.mp4')
-# cap = cv2.VideoCapture('./VOT-Woman.mp4')
-
-# take first frame of the video
-ret,frame = cap.read()
-# load the image, clone it, and setup the mouse callback function
-clone = frame.copy()
-cv2.namedWindow("First image")
-cv2.namedWindow("orientations")
-cv2.namedWindow("Hough transform")
-cv2.setMouseCallback("First image", define_ROI)
- 
-# keep looping until the 'q' key is pressed
-while True:
-	# display the image and wait for a keypress
-	cv2.imshow("First image", frame)
-	key = cv2.waitKey(1) & 0xFF
-	# if the ROI is defined, draw it!
-	if (roi_defined):
-		# draw a green rectangle around the region of interest
-		cv2.rectangle(frame, (r,c), (r+h,c+w), (0, 255, 0), 2)
-	# else reset the image...
-	else:
-		frame = clone.copy()
-	# if the 'q' key is pressed, break from the loop
-	if key == ord("q"):
-		break
- 
-track_window = (r,c,h,w)
-Rtable = {}
-for k in range(21):
-    Rtable[k]=[]
-# set up the ROI for tracking
-roi = frame[r:r+h,c:c+w]
-center_x = c+w/2
-center_y=r+h/2
-clone = cv2.cvtColor(clone, cv2.COLOR_BGR2GRAY)
-mod,ori = compute_grad(clone)
-ori+=90
-discret = 360/20
-for i in range(roi.shape[0]):
-    for j in range(roi.shape[1]):
-        if mod[r+i,c+j]>20:
-            Rtable[int(ori[r+i,c+j]/discret)].append((i-center_y+r,j-center_x+c))
+from utils import ROI, compute_grad, process_roi
 
 
-track_window = (r,c,h,w)
-# set up the ROI for tracking
-roi = frame[c:c+w, r:r+h]
-# conversion to Hue-Saturation-Value space
-# 0 < H < 180 ; 0 < S < 255 ; 0 < V < 255
-hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-# computation mask of the histogram:
-# Pixels with S<30, V<20 or V>235 are ignored 
-mask = cv2.inRange(hsv_roi, np.array((0.,30.,20.)), np.array((180.,255.,235.)))
-# Marginal histogram of the Hue component
-roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-# Histogram values are normalised to [0,255]
-cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+def process_video(path):
+    """
+    Processes a video by tracking an object within a defined region of interest (ROI).
 
-# Setup the termination criteria: either 10 iterations,
-# or move by less than 1 pixel
-term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+    Args:
+        path: The path to the video file.
+    """
+    cap = cv2.VideoCapture(path)
 
-cpt = 1
+    # Check if the video capture object was successfully created
+    if not cap.isOpened():
+        print(f"Error: Could not open video file {path}.")
+        return
 
-while(1):
-    key = cv2.waitKey(1)
-    ret, frame = cap.read()
-    if ret==True:
-        # print('center',center_x,center_y)
+    try:
+        # take first frame of the video
+        ret, frame = cap.read()
+
+        # load the image, clone it, and setup the mouse callback function
         clone = frame.copy()
-        clone = cv2.cvtColor(clone, cv2.COLOR_BGR2HSV)
-        mod,ori = compute_grad(clone[:,:,2])
-        ori+=90
-        H = np.zeros(ori.shape)
-        for l in range(ori.shape[0]):
-            for m in range(ori.shape[1]):
-                if mod[l,m] > 20:
-                    for ind in Rtable[int(ori[l,m]/discret)]:
-                        if l+ind[0]>= 0 and l+ind[0]<H.shape[0] and m+ind[1]>=0 and m+ind[1]<H.shape[1]:
-                            H[int(l+ind[0]),int(m+ind[1])]+=1
+        cv2.namedWindow("First image")
+        cv2.namedWindow("orientations")
+        cv2.namedWindow("Hough transform")
+        myROI = ROI()
+        cv2.setMouseCallback("First image", myROI.define_ROI)
+
+        # keep looping until the 'q' key is pressed
+        while True:
+            # display the image and wait for a keypress
+            cv2.imshow("First image", frame)
+            key = cv2.waitKey(1) & 0xFF
+            # if the ROI is defined, draw it!
+            frame = process_roi(frame, myROI, clone)
+            # if the 'q' key is pressed, break from the loop
+            if key == ord("q"):
+                break
+
+        track_window = (myROI.r, myROI.c, myROI.h, myROI.w)
+        Rtable = {k: [] for k in range(21)}
+
+        # set up the ROI for tracking
+        roi = frame[myROI.r : myROI.r + myROI.h, myROI.c : myROI.c + myROI.w]
+        center_x = myROI.c + myROI.w / 2
+        center_y = myROI.r + myROI.h / 2
+        clone = cv2.cvtColor(clone, cv2.COLOR_BGR2GRAY)
+        mod, ori = compute_grad(clone)
+        ori += 90
+        discret = 360 / 20
+
+        # Create indices for the entire ROI
+        roi_indices = np.indices(roi.shape)
+        i_indices, j_indices = roi_indices[0], roi_indices[1]
+
+        # Offset indices to match the position in the original image
+        i_indices += myROI.r
+        j_indices += myROI.c
+
+        # Flatten the indices for vectorized operations
+        i_flat = i_indices.flatten()
+        j_flat = j_indices.flatten()
+
+        # Apply the condition mod > 20
+        condition = mod[i_flat, j_flat] > 20
+
+        # Filter indices and orientations based on the condition
+        filtered_i = i_flat[condition]
+        filtered_j = j_flat[condition]
+        filtered_ori = ori[filtered_i, filtered_j]
+
+        # Compute displacements and update Rtable
+        displacements = np.stack([filtered_i - center_y, filtered_j - center_x], axis=-1)
+        for displacement, angle in zip(displacements, filtered_ori):
+            Rtable[int(angle / discret)].append(tuple(displacement))
+
+        # set up the ROI for tracking
+        roi = frame[myROI.c : myROI.c + myROI.w, myROI.r : myROI.r + myROI.h]
         
-        cv2.imshow("Hough transform", H/H.max())
+        # conversion to Hue-Saturation-Value space
+        # 0 < H < 180 ; 0 < S < 255 ; 0 < V < 255
+        hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
-        ret, track_window = cv2.meanShift(H, track_window, term_crit)
+        # computation mask of the histogram:
+        # Pixels with S<30, V<20 or V>235 are ignored
+        mask = cv2.inRange(
+            hsv_roi, np.array((0.0, 30.0, 20.0)), np.array((180.0, 255.0, 235.0))
+        )
+        # Marginal histogram of the Hue component
+        roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
+        
+        # Histogram values are normalised to [0,255]
+        cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
 
-        # Draw a blue rectangle on the current image
-        r,c,h,w = track_window
-        frame_tracked = cv2.rectangle(frame, (r,c), (r+h,c+w), (255,0,0) ,2)
-        cv2.imshow("First image", frame_tracked)
+        # Setup the termination criteria: either 10 iterations,
+        # or move by less than 1 pixel
+        term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
 
-        #display valid orientations
-        ori = cv2.cvtColor(ori.astype('uint8'), cv2.COLOR_GRAY2BGR)
-        ori[np.where(mod<20)] = [0,0,255]
-        cv2.imshow("orientations", ori)
-    else:
-        break
+        while True:
+            key = cv2.waitKey(1)
+            ret, frame = cap.read()
+            if not ret:
+                break
+            clone = frame.copy()
+            clone = cv2.cvtColor(clone, cv2.COLOR_BGR2HSV)
+            mod, ori = compute_grad(clone[:, :, 2])
+            ori += 90
+            H = np.zeros(ori.shape)
 
-cv2.destroyAllWindows()
-cap.release()
+            for l in range(ori.shape[0]):
+                for m in range(ori.shape[1]):
+                    if mod[l, m] > 20:
+                        for ind in Rtable[int(ori[l, m] / discret)]:
+                            if (
+                                l + ind[0] >= 0
+                                and l + ind[0] < H.shape[0]
+                                and m + ind[1] >= 0
+                                and m + ind[1] < H.shape[1]
+                            ):
+                                H[int(l + ind[0]), int(m + ind[1])] += 1
+
+            cv2.imshow("Hough transform", H / H.max())
+
+            ret, track_window = cv2.meanShift(H, track_window, term_crit)
+
+            # Draw a blue rectangle on the current image
+            r, c, h, w = track_window
+            frame_tracked = cv2.rectangle(frame, (r, c), (r + h, c + w), (255, 0, 0), 2)
+            cv2.imshow("First image", frame_tracked)
+
+            # display valid orientations
+            ori = cv2.cvtColor(ori.astype("uint8"), cv2.COLOR_GRAY2BGR)
+            ori[np.where(mod < 20)] = [0, 0, 255]
+            cv2.imshow("orientations", ori)
+    
+    except KeyboardInterrupt:
+        print("Interrupted by user, releasing resources.")
+    
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    process_video(VIDEO_PATHS[0])
