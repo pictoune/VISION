0f9b08267cc9780cad7a917532b772286db5a51a
Victor Piriou <65340859+pictoune@users.noreply.github.com>
2023-11-21 20:34:40 +0100

    Update hough.py

--- PW_object_tracking/code/hough.py
+++ PW_object_tracking/code/hough.py
@@ -122,13 +122,4 @@ def process_video(path):
 
 
 if __name__ == "__main__":
-    paths = {
-        0: "data/VOT-Ball.mp4",
-        1: "data/Antoine_Mug.mp4",
-        2: "data/VOT-Sunshade.mp4",
-        3: "data/VOT-Basket.mp4",
-        4: "data/VOT-Car.mp4",
-        5: "data/VOT-Woman.mp4",
-    }
-
     process_video(VIDEO_PATHS[0])
